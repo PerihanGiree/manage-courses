1fc9a7122330ecb09b5805fbc5379fd86e77d33b
Sezer67 <kenarsezer08@gmail.com>
2023-07-02 23:18:14 +0300

    edit - add funcs

--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -1,15 +1,23 @@
 export type User = {
-  id: number;
-  firstName: string;
-  lastName: string;
-  email: string;
-  phone: string;
-  domain: string;
-  company: {
-    name: string;
-    title: string;
-    department: string;
-  };
-  image: string;
-  // Diğer kullanıcı özellikleri buraya eklenebilir
+	id: number;
+	firstName: string;
+	lastName: string;
+	email: string;
+	phone: string;
+	domain: string;
+	company: {
+		name: string;
+		title: string;
+		department: string;
+	};
+	image: string;
+	// Diğer kullanıcı özellikleri buraya eklenebilir
+};
+
+export type StudentFormData = {
+	firstName: string;
+	lastName: string;
+	email: string;
+	phone: string;
+	companyName: string;
 };
