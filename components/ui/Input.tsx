c7a7cc031627a2f3c659f574315960f6d333697c
Perihan Gire <56370086+PerihanGiree@users.noreply.github.com>
2023-07-03 16:04:58 +0300

    search-lodash

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -3,6 +3,8 @@ import { InputType } from "zlib";
 
 type PropsType = InputHTMLAttributes<any> & {
   label: string;
+  onKeyPress?: () => void;
+  image: string;
 };
 
 const Input: React.FC<PropsType> = (props) => {
@@ -11,13 +13,15 @@ const Input: React.FC<PropsType> = (props) => {
       <span className="text-[14px] font-medium text-lightGray">
         {props.label}
       </span>
-      <input
-        className={`w-full mt-3 rounded-[4px] text-[12px] ${
-          props.value ? "text-[#6c6c6c]" : "text-[#CDCDCD]"
-        } border border-[#E5E5E5] h-11 p-4 focus:outline-none mb-5`}
-        type="text"
-        {...props}
-      />
+      <div className="flex flex-row">
+        <input
+          className={`w-full mt-3 rounded-[4px] text-[12px] ${
+            props.value ? "text-[#6c6c6c]" : "text-[#CDCDCD]"
+          } border border-[#E5E5E5] h-11 p-4 focus:outline-none mb-5`}
+          type="text"
+          {...props}
+        />
+      </div>
     </div>
   );
 };
