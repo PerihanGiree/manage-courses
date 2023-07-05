adc380c41830ce7de45709840ad9e6b9c8a7d47d
Perihan Gire <56370086+PerihanGiree@users.noreply.github.com>
2023-07-05 12:02:34 +0300

    buton

--- components/UserDetail.tsx
+++ components/UserDetail.tsx
@@ -3,6 +3,7 @@ import { userTypes } from "@/src/types";
 import React, { useState, useEffect } from "react";
 import UserCard from ".././public/user/id-card.png";
 import email from ".././public/user/mail.png";
+import { userInfo } from "os";
 
 const UserDetail: React.FC<{ id: number }> = ({ id }) => {
   const [user, setUser] = useState<userTypes.UserInfo | undefined>();
@@ -25,146 +26,38 @@ const UserDetail: React.FC<{ id: number }> = ({ id }) => {
 
   return (
     <div className="shadow-md  w-[98%] h-[90%]  bg-[#f2f2f2] rounded-md p-2 m-4 ">
-      <h1 className="font-bold ml-5 text-xl pl-6 text-primary pt-5 ">
-        User Details
-      </h1>
-      <div className="flex flex-col justify-center items-center ">
-        <img
-          src={user.image}
-          alt={user.image}
-          className="w-40 h-40 rounded-full border-y-2 shadow-md bg-primary"
-        />
-      </div>
-      <div className="flex flex-row justify-between ">
-        <div className="flex flex-col justify-center items-start ml-80 pl-8 pt-4">
-          <div className="flex flex-row justify-between">
-            <img
-              src={UserCard}
-              alt="usercard"
-              className="w-[80px] h-[80px] p-1 text-primary"
-            />
-
-            <div className="flex flex-col mt-3 p-2">
-              <span>
-                <span className="font-semibold">Firstname:</span>
-                {user.firstName}
-              </span>
-              <span className="h-1 bg-[#ccc] w-[100%]"></span>
-              <span>
-                <span className="font-semibold">Lastname:</span>
-                {user.lastName}
-              </span>
-              <span className="h-1 bg-[#ccc] w-[100%]"></span>
-              {/** */}
-            </div>
-          </div>
-
-          <span>
-            <span className="font-semibold">Email:</span>
-            {user.email}
-          </span>
-
-          <span className="h-1 bg-[#ccc] w-[100%]"></span>
-
-          <span>
-            <span className="font-semibold">Phone:</span>
-            {user.phone}
-          </span>
-          <span className="h-1 bg-[#ccc] w-[100%]"></span>
-
-          <span>
-            <span className="font-semibold">Company Name:</span>
-            {user.company.name}
-          </span>
-          <span className="h-1 bg-[#ccc] w-[100%]"></span>
-          <span>
-            <span className="font-semibold">Company Address:</span>
-            {user.company.address.address}
-          </span>
-          <span className="h-1 bg-[#ccc] w-[100%]"></span>
-          <span>
-            <span className="font-semibold">Username:</span>
-            {user.username}
-          </span>
-          <span className="h-1 bg-[#ccc] w-[100%]"></span>
-          <span>
-            <span className="font-semibold">Birthday Date:</span>
-            {user.birthDate}
-          </span>
-          <span className="h-1 bg-[#ccc] w-[100%]"></span>
-          <span>
-            <span className="font-semibold">Blood Group:</span>
-            {user.bloodGroup}
-          </span>
-          <span className="h-1 bg-[#ccc] w-[100%]"></span>
-          <span>
-            <span className="font-semibold">Height:</span>
-            {user.height}
-          </span>
-          <span className="h-1 bg-[#ccc] w-[100%]"></span>
-          <span>
-            <span className="font-semibold">Weight:</span>
-            {user.weight}
-          </span>
-          <span className="h-1 bg-[#ccc] w-[100%]"></span>
-          <span>
-            <span className="font-semibold">Eye Color:</span>
-            {user.eyeColor}
-          </span>
-          <span className="h-1 bg-[#ccc] w-[100%]"></span>
-          <span>
-            <span className="font-semibold">Hair Color:</span>
-            {user.hair.color}
-          </span>
-          <span className="h-1 bg-[#ccc] w-[100%]"></span>
-          <span>
-            <span className="font-semibold">Hair Type:</span>
-            {user.hair.type}
-          </span>
-          <span className="h-1 bg-[#ccc] w-[100%]"></span>
+      <h2 className="text-2xl text-primary font-semibold  m-5">User Detail</h2>
+      <div className="flex flex-row items-center justify-center">
+        <div className="w-[50%] h-full items-center justify-center  mt-20 pl-28">
+          <p>
+            <strong>Ad:</strong> {user.firstName}
+            {user.lastName}
+          </p>
+          <p>
+            <strong>Kullanıcı Adı:</strong> {user.username}
+          </p>
+          <p>
+            <strong>Email:</strong> {user.email}
+          </p>
+          <p>
+            <strong>Telefon:</strong> {user.phone}
+          </p>
+          <p>
+            <strong>Website:</strong> {user.domain}
+          </p>
+          <h2 className="text-lg font-semibold mt-4 mb-2">Adres</h2>
+          <p>
+            <strong>Sokak:</strong> {user.address.address}
+          </p>
+          <p>
+            <strong>Şehir:</strong> {user.address.city}
+          </p>
+          <p>
+            <strong>Posta Kodu:</strong> {user.address.postalCode}
+          </p>
         </div>
-        <div className="flex flex-col mr-80 pt-4">
-          <span>
-            <span className="font-semibold">Domain:</span>
-            {user.domain}
-          </span>
-          <span className="h-1 bg-[#ccc] w-[100%]"></span>
-          <span>
-            <span className="font-semibold">Ip Address:</span>
-            {user.ip}
-          </span>
-          <span className="h-1 bg-[#ccc] w-[100%]"></span>
-          <span>
-            <span className="font-semibold">Address:</span>
-            <p>{user.address.address}</p>
-            <p> {user.address.city}</p>
-            <p>
-              {user.address.coordinates.lat}-{user.address.coordinates.lng}
-            </p>
-            <p>{user.address.postalCode}</p>
-
-            <p>{user.address.state}</p>
-          </span>
-          <span className="h-1 bg-[#ccc] w-[100%]"></span>
-          <span>
-            <span className="font-semibold">Mac Address:</span>
-            {user.macAddress}
-          </span>
-          <span className="h-1 bg-[#ccc] w-[100%]"></span>
-          <span>
-            <span className="font-semibold">University:</span>
-            {user.university}
-          </span>
-          <span className="h-1 bg-[#ccc] w-[100%]"></span>
-          <span>
-            <span className="font-semibold">Bank Information:</span>
-            <p>Card Expire-{user.bank.cardExpire}</p>
-            <p>Card Number- {user.bank.cardNumber}</p>
-            <p> Card Type-{user.bank.cardType}</p>
-            <p>Currency- {user.bank.currency}</p>
-            <p>Iban-{user.bank.iban}</p>
-          </span>
-          <span className="h-1 bg-[#ccc] w-[100%]"></span>
+        <div className="w-[50%] h-full">
+          <img src={user.image} alt={user.image} />
         </div>
       </div>
     </div>
--- pages/students/index.tsx
+++ pages/students/index.tsx
@@ -1,207 +1,205 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import Button from '@/components/ui/Button';
-import React, { useEffect, useState } from 'react';
-import UserTable from '@/components/ui/UserTable';
-import { userTypes } from '@/src/types';
-import { paginationDatas } from '@/src/constans';
-import { useRouter } from 'next/router';
-import { routeHelper } from '@/src/helpers';
-import CustomModal from '@/components/ui/Modal';
-import StudentForm from '@/components/StudentForm';
-import Search from '../../public/search 1.svg';
-import InputWithIcon from '@/components/ui/InputWithIcon';
+import Button from "@/components/ui/Button";
+import React, { useEffect, useState } from "react";
+import UserTable from "@/components/ui/UserTable";
+import { userTypes } from "@/src/types";
+import { paginationDatas } from "@/src/constans";
+import { useRouter } from "next/router";
+import { routeHelper } from "@/src/helpers";
+import CustomModal from "@/components/ui/Modal";
+import StudentForm from "@/components/StudentForm";
+import Search from "../../public/search 1.svg";
+import InputWithIcon from "@/components/ui/InputWithIcon";
 
 const TableTitle = [
-	{
-		id: 0,
-		title: '', // for images column,
-		className: 'w-[12%]'
-	},
-	{
-		id: 1,
-		title: 'Name',
-		className: 'w-[14%]'
-	},
-	{
-		id: 2,
-		title: 'Email',
-		className: 'w-[16%]'
-	},
-	{
-		id: 3,
-		title: 'Phone',
-		className: 'w-[14%]'
-	},
-	{
-		id: 4,
-		title: 'Website',
-		className: 'w-[16%]'
-	},
-	{
-		id: 5,
-		title: 'Company Name',
-		className: 'w-[12%]'
-	},
-	{
-		id: 6,
-		title: '', // for actions column
-		className: 'w-[16%]'
-	}
+  {
+    id: 0,
+    title: "", // for images column,
+    className: "w-[12%]",
+  },
+  {
+    id: 1,
+    title: "Name",
+    className: "w-[14%]",
+  },
+  {
+    id: 2,
+    title: "Email",
+    className: "w-[16%]",
+  },
+  {
+    id: 3,
+    title: "Phone",
+    className: "w-[14%]",
+  },
+  {
+    id: 4,
+    title: "Website",
+    className: "w-[16%]",
+  },
+  {
+    id: 5,
+    title: "Company Name",
+    className: "w-[12%]",
+  },
+  {
+    id: 6,
+    title: "", // for actions column
+    className: "w-[16%]",
+  },
 ];
 
 const Students = () => {
-	const [user, setUser] = useState<userTypes.User[]>([]);
-	const [inputValue, setInputValue] = useState('');
-	const [total, setTotal] = useState<number>(0);
-	const [loading, setLoading] = useState<boolean>(false);
-	const [modalVisible, setModalVisible] = useState<boolean>(false);
+  const [user, setUser] = useState<userTypes.User[]>([]);
+  const [inputValue, setInputValue] = useState("");
+  const [total, setTotal] = useState<number>(0);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
 
-	const router = useRouter();
-	let timer: NodeJS.Timeout | null = null;
-	useEffect(() => {
-		const { limit, offset, search } = router.query;
-		fethMoreData({
-			limit: limit ? limit.toString() : undefined,
-			offset: offset ? offset.toString() : undefined,
-			search: search ? search.toString() : undefined
-		});
-	}, [router.query]);
+  const router = useRouter();
+  let timer: NodeJS.Timeout | null = null;
+  useEffect(() => {
+    const { limit, offset, search } = router.query;
+    fethMoreData({
+      limit: limit ? limit.toString() : undefined,
+      offset: offset ? offset.toString() : undefined,
+      search: search ? search.toString() : undefined,
+    });
+  }, [router.query]);
 
-	const fethMoreData = async ({
-		limit,
-		offset,
-		search
-	}: {
-		limit?: string;
-		offset?: string;
-		search?: string;
-	}) => {
-		const query: any = {};
-		let url = '';
-		query.limit = limit || paginationDatas.perPage;
-		if (offset) {
-			query.skip = offset;
-		}
-		if (search) {
-			query.q = search;
-			url = routeHelper.addQueryPArameters(
-				'https://dummyjson.com/users/search',
-				query
-			);
-		} else {
-			// bu endpoint search ü kapsamıyor
-			url = routeHelper.addQueryPArameters(
-				'https://dummyjson.com/users',
-				query
-			);
-		}
+  const fethMoreData = async ({
+    limit,
+    offset,
+    search,
+  }: {
+    limit?: string;
+    offset?: string;
+    search?: string;
+  }) => {
+    const query: any = {};
+    let url = "";
+    query.limit = limit || paginationDatas.perPage;
+    if (offset) {
+      query.skip = offset;
+    }
+    if (search) {
+      query.q = search;
+      url = routeHelper.addQueryPArameters(
+        "https://dummyjson.com/users/search",
+        query
+      );
+    } else {
+      // bu endpoint search ü kapsamıyor
+      url = routeHelper.addQueryPArameters(
+        "https://dummyjson.com/users",
+        query
+      );
+    }
 
-		setLoading(true);
-		try {
-			const data = await fetch(url).then((res) => res.json());
-			setUser(data.users);
-			setTotal(data.total);
-		} catch (error) {
-			console.log(error);
-		} finally {
-			setLoading(false);
-		}
-	};
+    setLoading(true);
+    try {
+      const data = await fetch(url).then((res) => res.json());
+      setUser(data.users);
+      setTotal(data.total);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-	const handleInputChange = (
-		event: React.ChangeEvent<HTMLInputElement>
-	) => {
-		setInputValue(event.target.value);
-	};
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
 
-	const userUpdate = (editedUser: userTypes.User) => {
-		setUser((prev) =>
-			prev.map((item) => {
-				if (item.id === editedUser.id) {
-					return editedUser;
-				}
-				return item;
-			})
-		);
-	};
+  const userUpdate = (editedUser: userTypes.User) => {
+    setUser((prev) =>
+      prev.map((item) => {
+        if (item.id === editedUser.id) {
+          return editedUser;
+        }
+        return item;
+      })
+    );
+  };
 
-	const userDelete = (id: number) => {
-		setUser((prev) => prev.filter((item) => item.id !== id));
-	};
+  const userDelete = (id: number) => {
+    setUser((prev) => prev.filter((item) => item.id !== id));
+  };
 
-	//Search
-	const handleSearch = async () => {
-		// Arama işlemini gerçekleştirirken debounce mekanizması kullanılmayacak
-		const { pathname, query } = router;
-		const updatedQuery = { ...query, search: inputValue, offset: 0 };
-		router.push({
-			pathname,
-			query: updatedQuery
-		});
-	};
-	const debounceSearch = () => {
-		if (timer) {
-			clearTimeout(timer);
-		}
-		timer = setTimeout(() => {
-			handleSearch();
-			timer = null;
-		}, 500);
-	};
-	useEffect(() => {
-		debounceSearch();
+  //Search
+  const handleSearch = async () => {
+    // Arama işlemini gerçekleştirirken debounce mekanizması kullanılmayacak
+    const { pathname, query } = router;
+    const updatedQuery = { ...query, search: inputValue, offset: 0 };
+    router.push({
+      pathname,
+      query: updatedQuery,
+    });
+  };
+  const debounceSearch = () => {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(() => {
+      handleSearch();
+      timer = null;
+    }, 500);
+  };
+  useEffect(() => {
+    debounceSearch();
 
-		return () => {
-			if (timer) {
-				clearTimeout(timer);
-			}
-		};
-	}, [inputValue]);
-	return (
-		<div className="bg-[#f8f8f8] h-[calc(100vh-60px)] px-8 pt-5">
-			{loading && (
-				<div className="w-full h-full flex justify-center items-center">
-					Loading...
-				</div>
-			)}
-			{/* Header  */}
-			<div className=" flex justify-between">
-				<div className="font-bold text-[22px]">Students List</div>
-				<div className="flex flex-row items-center justify-center ">
-					<InputWithIcon
-						value={inputValue}
-						onChange={handleInputChange}
-						imgSrc={Search}
-						position="right"
-						className="w-52"
-						placeholder="Search..."
-					/>
-					<Button
-						className="ml-6 px-3"
-						onClick={() => setModalVisible(true)}
-						title="Add new Student"
-					/>
-				</div>
-			</div>
+    return () => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+    };
+  }, [inputValue]);
+  return (
+    <div className="bg-[#f8f8f8] h-[calc(100vh-60px)] px-8 pt-5">
+      {loading && (
+        <div className="w-full h-full flex justify-center items-center">
+          Loading...
+        </div>
+      )}
+      {/* Header  */}
+      <div className=" flex justify-between">
+        <div className="font-bold text-[22px]">Students List</div>
+        <div className="flex flex-row items-center justify-center ">
+          <InputWithIcon
+            value={inputValue}
+            onChange={handleInputChange}
+            imgSrc={Search}
+            position="right"
+            className="w-52"
+            placeholder="Search..."
+          />
+          <Button
+            className="ml-6 px-3"
+            onClick={() => setModalVisible(true)}
+            title="Add new Student"
+          />
+        </div>
+      </div>
 
-			<div className="w-full h-[1px] bg-[#E5E5E5] mt-2" />
+      <div className="w-full h-[1px] bg-[#E5E5E5] mt-2" />
 
-			<UserTable
-				headerTitles={TableTitle}
-				users={user}
-				total={total}
-				userUpdateFunc={userUpdate}
-				userDeleteFunc={userDelete}
-			/>
+      <UserTable
+        headerTitles={TableTitle}
+        users={user}
+        total={total}
+        userUpdateFunc={userUpdate}
+        userDeleteFunc={userDelete}
+      />
 
-			<CustomModal
-				visible={modalVisible}
-				setVisible={setModalVisible}
-				headerData={{ title: 'Add New Student' }}
-			>
-				<StudentForm isEdit={false} />
-			</CustomModal>
-		</div>
-	);
+      <CustomModal
+        visible={modalVisible}
+        setVisible={setModalVisible}
+        headerData={{ title: "Add New Student" }}
+      >
+        <StudentForm isEdit={false} />
+      </CustomModal>
+    </div>
+  );
 };
 export default Students;
